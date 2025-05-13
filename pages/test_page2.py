1a04a87d930ce926c38893a172dda7a1c503c030
John Mazzie <jmazzie@micron.com>
2025-05-12 21:06:26 -0500

    changed_function_names

--- pages/test_page2.py
+++ pages/test_page2.py
@@ -1,6 +1,6 @@
 import streamlit as st
-# def run():
+def main_function():
 st.write("This is a test page #2")
 
-# if __name__ == '__page__':
-#     run()
+if __name__ == '__page__':
+    main_function()
