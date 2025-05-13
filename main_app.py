16aaaa1c1216b5bf81a8c2c863a1df208c79fef5
John Mazzie <jmazzie@micron.com>
2025-05-12 21:01:13 -0500

    Test Pages created

--- main_app.py
+++ main_app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
-st.write("Hello")
+welcome_page = st.Page("pages/welcome.py")
+test_page1 = st.Page("pages/test_page1.py")
+test_page2 = st.Page("pages/test_page2.py")
+
+pg = st.navigation([welcome_page, test_page1, test_page2])
+pg.run()
